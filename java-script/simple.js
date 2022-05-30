0070f4056e6a547922d86738b7d048eceee75a19
Oman Kumar <kumaroman4@gmail.com>
2022-05-30 22:22:32 +0530

    java script started

---
+++ java-script/simple.js
@@ -0,0 +1,13 @@
+console.time('time taken')
+console.log('hey oman this is java script');
+console.log(34*4);
+console.table({oman:'name' , marks:34});
+console.timeEnd('time taken')
+
+// variables in js
+// var let const
+var oman='Human';
+console.log(oman)
+var x;
+x=23;
+console.log(x)
