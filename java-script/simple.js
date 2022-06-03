6b4f1f63cf076207e53f2232f680ea5e2d38a4a4
Oman Kumar <kumaroman4@gmail.com>
2022-06-03 21:17:15 +0530

    java script

--- java-script/simple.js
+++ java-script/simple.js
@@ -6,8 +6,68 @@ console.timeEnd('time taken')
 
 // variables in js
 // var let const
-var oman='Human';
-console.log(oman)
-var x;
-x=23;
-console.log(x)
+// var oman='Human';
+// console.log(oman)
+// var x;
+// x=23;
+// console.log(x)
+
+// // strings 
+// let x=`"this" is a string`;
+// let y=`oman is good`
+
+// // string methods
+// console.log(x.slice(0,5));
+// console.log(x.substring(2,5));
+
+// // for substr need length
+// console.log(y.substr(2,5));
+// console.log(y.replace('good','better'));
+// console.log (x.concat(' ' ,y));
+
+// console.log(x.split(" "));
+
+// // string methods never change the original string
+// console.log(x);
+// console.log(y);
+
+
+// Arrays
+
+let cars= new Array("Volvo","BMW","Maruti","TATA");
+const bus= ["mini","52_seater", "normal"];
+let num =[2,5,54,34,534,6,23];
+/*
+// array methods
+console.log(num.join('+'));
+console.log(cars.toString());
+
+console.log(num.pop());    // it changes the original array
+console.log(num)
+
+console.log(cars.push("Audi")) // it changes the original array
+console.log(cars)
+
+console.log(num.shift()) // it changes the original array
+console.log(num.unshift(10))
+console.log(num)
+
+console.log(bus.splice(1,0,"Travelor","mini")) // it changes the original array
+console.log(bus.splice(2,1,"big-mini"))
+console.log(bus)
+
+console.log(cars.sort())   // it changes the original array
+console.log(cars)
+
+console.log(num.sort(function(a,b){return a-b}))  // it changes the original array
+console.log(num.sort(function(a,b){return b-a}))
+console.log(num)
+
+
+let fav=cars.slice(0,3) // it creates a new array without changing original one
+console.log(fav)
+console.log(cars)
+
+console.log(cars.concat(bus))  // doesn't change the array
+console.log(cars)
+*/
